8e2bbc4460eec1a382490b01fff45672584e390f
abcode <mabsattar@proton.me>
2024-07-03 04:59:34 +0530

    added jsx for the UI

--- src/components/createSCW.tsx
+++ src/components/createSCW.tsx
@@ -3,6 +3,7 @@ import { useEffect, useRef, useState } from "react";
 import { useAccount } from "wagmi";
 import { isAddress } from "ethers/lib/utils";
 import { useRouter } from "next/navigation";
+import Icon from "@/components/icon";
 
 export default function CreateSCW() {
   const [signers, setSigners] = useState<string[]>([]);
@@ -84,5 +85,64 @@ export default function CreateSCW() {
     }
   };
 
-  return null;
+  return (
+    <main className="flex flex-col py-6 items-center gap-5">
+      <h1 className="text-5xl font-bold">Create New Wallet</h1>
+      <p className="text-gray-400">
+        Enter the signer addresses for this account
+      </p>
+      <div className="flex flex-col gap-6 max-w-sm w-full">
+        {signers.map((signer, index) => (
+          <div key={signer} className="flex items-center gap-4">
+            <input
+              type="text"
+              className="rounded-lg p-2 w-full text-slate-700"
+              placeholder="0x0"
+              value={signer}
+              ref={index === signers.length - 1 ? lastInput : null}
+              onKeyDown={(event) => {
+                if (event.key === "Enter") {
+                  addNewSigner();
+                } else if (event.key === "Backspace") {
+                  removeSigner(index);
+                }
+              }}
+              onChange={(event) => {
+                const newSigners = [...signers];
+                newSigners[index] = event.target.value;
+                setSigners(newSigners);
+              }}
+            />
+
+            {index > 0 && (
+              <div
+                className="hover:scale-105 cursor-pointer"
+                onClick={() => removeSigner(index)}
+              >
+                <Icon type="xmark" />
+              </div>
+            )}
+          </div>
+        ))}
+        {loading ? (
+          <div className="animate-spin rounded-full h-6 w-6 border-b-2 border-l-white items-center justify-center mx-auto" />
+        ) : (
+          <div className="flex items-center justify-between">
+            <button
+              className="bg-blue-500 mx-auto hover:bg-blue-700 disabled:bg-blue-500/50 disabled:hover:bg-blue-500/50 hover:transition-colors text-white font-bold py-2 w-fit px-4 rounded-lg"
+              onClick={addNewSigner}
+            >
+              Add New Signer
+            </button>
+            <button
+              className="bg-blue-500 mx-auto hover:bg-blue-700 disabled:bg-blue-500/50 disabled:hover:bg-blue-500/50 hover:transition-colors text-white font-bold py-2 w-fit px-4 rounded-lg"
+              onClick={onCreateSCW}
+            >
+              Create New Wallet
+            </button>
+          </div>
+        )}
+      </div>
+    </main>
+  );
 }
