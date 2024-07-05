457cd469d99bb0c8e3ef5aac895e03047948f2b5
abcode <mabsattar@proton.me>
2024-07-06 04:27:32 +0530

    added jsx to walletList comp

--- src/components/walletList.tsx
+++ src/components/walletList.tsx
@@ -1,5 +1,7 @@
 import { Wallet } from "@prisma/client";
 import { useEffect, useState } from "react";
+import Icon from "./icon";
+import Link from "next/link";
 
 // Define a new type that includes all the fields within Wallet
 // as well as _count which is the number of transactions corresponding to a wallet
@@ -19,4 +21,70 @@ export default function WalletList({ address }: { address: string }) {
       .then((response) => response.json())
       .then((data) => setWallets(data));
   }, [address]);
+
+
+return (
+    <main className="flex flex-col justify-center items-center gap-5">
+      <h1 className="text-5xl font-bold">Your Wallets</h1>
+
+      {wallets.length === 0 ? (
+        <div className="flex justify-center items-center border-2 border-dashed p-6 rounded-lg">
+          <p className="text-lg">
+            You currently have no smart contract wallets.
+          </p>
+        </div>
+      ) : (
+        <div className="grid grid-cols-2 gap-4">
+          {wallets.map((wallet) => (
+            <div
+              key={wallet.id}
+              className="flex flex-col border border-gray-800 rounded-lg"
+            >
+              <Link
+                className="text-gray-300 font-medium"
+                href={`/${wallet.address}`}
+              >
+                <div className="bg-gray-800 flex justify-between gap-4 items-center rounded-t-lg p-2 font-mono">
+                  {wallet.address}
+                  <p className="text-gray-300">
+                    Pending Txns: {wallet._count.transactions}
+                  </p>
+                  <div className="bg-gray-300 rounded-full items-center px-2 py-1 flex gap-2">
+                    {wallet.isDeployed ? (
+                      <div className="h-2 w-2 rounded-full bg-green-400" />
+                    ) : (
+                      <div className="h-2 w-2 rounded-full bg-red-500" />
+                    )}
+
+                    <p className="text-gray-800 text-sm font-medium">
+                      {wallet.isDeployed ? "Deployed" : "Not Deployed"}
+                    </p>
+                  </div>
+                </div>
+              </Link>
+
+              <div className="flex flex-col py-2 divide-y divide-gray-600">
+                {wallet.signers.length > 0 && wallet.signers.map((signer, idx) => (
+                  <div
+                    key={`${wallet.address}-${signer}`}
+                    className="px-4 flex items-center justify-center gap-2 py-2"
+                  >
+                    <Icon type="user" />
+                    <p className="text-gray-300 font-mono">{signer}</p>
+                    <Link
+                      href={`https://sepolia.etherscan.io/address/${signer}`}
+                      target="_blank"
+                      className="text-blue-500 hover:text-blue-600"
+                    >
+                      Etherscan
+                    </Link>
+                  </div>
+                ))}
+              </div>
+            </div>
+          ))}
+        </div>
+      )}
+    </main>
+  );
 }
